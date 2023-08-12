766ea9e2e9a47fab475f6017e4e369186b78d131
J2K4UNM <giorgikatamadze718@gmail.com>
2023-08-12 16:30:42 +0400

    main page arrangement

--- src/script/ApiServices/apiService.js
+++ src/script/ApiServices/apiService.js
@@ -1,11 +1,12 @@
+const apiKey = "325af8d6e4b91e2f374e02a26776548c"; // Replace with your actual API key
+
 // create axios instance
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjVhZjhkNmU0YjkxZTJmMzc0ZTAyYTI2Nzc2NTQ4YyIsInN1YiI6IjY0ZDYxNjVkZGI0ZWQ2MDBlMmI2NTY3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.thha-OnxEpkeBWXagT8NzGQNjpV9_9OUKGeQp5v_GXk",
-  },
+      "Bearer 325af8d6e4b91e2f374e02a26776548c"}
 });
 
 // Define api endpoints
@@ -13,4 +14,4 @@ export const getMovies = (endpoint) => instance.get(endpoint);
 export const getActors = () => instance.get("/person/popular");
 export const getTvShows = () => instance.get("/trending/movie/day");
 
-
+export const generateImageUrl = (path) => instance.defaults.baseURL + path;
--- src/script/script.js
+++ src/script/script.js
@@ -1,20 +1,39 @@
-import { getMovies } from "../script/ApiServices/apiService.js";
+import { getMovies, generateImageUrl } from "../script/ApiServices/apiService.js";
 
 async function fetchData() {
   try {
     const resMovies = await getMovies("/movie/popular");
-    const dataMovies = resMovies.data;
+    const dataMovies = resMovies.data.results;
     console.log("Popular:", dataMovies);
 
     const resTopMovies = await getMovies("/movie/top_rated");
-    const dataTopMovies = resTopMovies.data;
+    const dataTopMovies = resTopMovies.data.results;
     console.log("Top movies:", dataTopMovies);
 
     const resUpcomingMovies = await getMovies("/movie/upcoming");
-    const dataUpcomingMovies = resUpcomingMovies.data;
+    const dataUpcomingMovies = resUpcomingMovies.data.results;
     console.log("Upcoming:", dataUpcomingMovies);
+    
+    displayMovies(dataMovies, dataTopMovies, dataUpcomingMovies);
+    
   } catch (error) {
     console.error("Error:", error);
   }
 }
+
+async function displayMovies(dataMovies, dataTopMovies, dataUpcomingMovies) {
+  const popularMoviesDiv = document.querySelector('#popularMovies');
+  const imgUrl = generateImageUrl("/movie/popular");
+  const topImgUrl = generateImageUrl("/movie/top_rated");
+  const upcomingImgUrl = generateImageUrl("/movie/upcoming")
+
+  popularMoviesDiv.innerHTML = dataMovies.map((movie) => `
+   <div>
+      <img src="${imgUrl + movie.poster_path}">
+   </div>
+  `).join('');
+  
+
+}
+
 fetchData();
